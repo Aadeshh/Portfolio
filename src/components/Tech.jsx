2612fbc5ac56546fcaf7dc66b5ea1bccfc63e507
Aadesh Thoppae <aadesh@Aadeshs-MacBook-Pro.local>
2024-07-14 00:05:50 -0700

    final changes

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,7 +4,7 @@ import { FaGolang } from "react-icons/fa6";
 import { FaPython } from "react-icons/fa";
 import { FaJava } from "react-icons/fa";
 import { SiCplusplus } from "react-icons/si";
-
+import pythonLogo from "../assets/python-logo-only.png"
 
 
 const Tech = () => {
@@ -13,7 +13,7 @@ const Tech = () => {
         <h2 className='my-20 text-center text-4xl'>Technologies</h2>
         <div className='flex flex-wrap items-center gap-4'>
             <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-                <FaPython className='text-7xl text-yellow-300'/>
+                <img className='w-18 h-20'src={pythonLogo}/>
             </div>
             <div className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <FaGolang className='text-7xl text-blue-400'/>
