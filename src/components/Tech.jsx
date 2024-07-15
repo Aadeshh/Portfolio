949c0fee1592101752132afd26a5a3bc37644b58
Aadesh Thoppae <aadesh@Aadeshs-MacBook-Pro.local>
2024-07-15 12:02:42 -0700

    added other technologies

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import {RiReactjsLine} from "react-icons/ri";
 import { FaGolang } from "react-icons/fa6";
-import { FaPython } from "react-icons/fa";
 import { FaJava } from "react-icons/fa";
 import { SiCplusplus } from "react-icons/si";
 import pythonLogo from "../assets/python-logo-only.png"
 
 
 const Tech = () => {
+    const [showMore, setShowMore] = React.useState(false);
+
+    const handleToggle = () => {
+        setShowMore(!showMore);
+    };
   return (
     <div className='border-b border-neutral-50-800 pb-24'>
         <h2 className='my-20 text-center text-4xl'>Technologies</h2>
@@ -30,6 +34,17 @@ const Tech = () => {
             </div>
 
         </div>
+        <button className='mt-10 px-4 py-2 bg-indigo-700 text-white rounded-md' onClick={handleToggle}>
+            {showMore ? 'Hide Other Technologies' : 'Show Other Technologies'}
+        </button>
+
+        {showMore && (
+            <div className='mt-4'>
+                <p className="text-center text-lg max-w-2xl mx-auto">
+                   C, Ruby, JavaScript, Git, MATLAB, R, C#, HTML, CSS, Tensorflow, Pandas, Jira, Scikit-learn, Docker
+                </p>
+            </div>
+        )}
     </div>
   )
 }
